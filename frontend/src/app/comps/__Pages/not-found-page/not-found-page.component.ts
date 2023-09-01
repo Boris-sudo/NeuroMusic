7b843a7c39fae338bb5650666fca0c5f86bf473b
boriskiva <boriskiva3@gmail.com>
2023-09-01 09:36:59 +0300

    making new version of page not found

--- frontend/src/app/comps/__Pages/not-found-page/not-found-page.component.ts
+++ frontend/src/app/comps/__Pages/not-found-page/not-found-page.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {TimeInterval} from "rxjs";
 
 @Component({
   selector: 'app-not-found-page',
@@ -11,6 +12,50 @@ export class NotFoundPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    function randomNum() {
+      "use strict";
+      return Math.floor(Math.random() * 9) + 1;
+    }
+    let loop1:any,
+      loop2:any,
+      loop3:any,
+      time = 30,
+      i = 0,
+      number,
+      selector3 = document.querySelector(".thirdDigit")!,
+      selector2 = document.querySelector(".secondDigit")!,
+      selector1 = document.querySelector(".firstDigit")!;
+    loop3 = setInterval(function () {
+      "use strict";
+      if (i > 40) {
+        clearInterval(loop3);
+        selector3.textContent = '4';
+      } else {
+        selector3.textContent = String(randomNum());
+        i++;
+      }
+    }, time);
+    loop2 = setInterval(function () {
+      "use strict";
+      if (i > 80) {
+        clearInterval(loop2);
+        selector2.textContent = '0';
+      } else {
+        selector2.textContent = String(randomNum());
+        i++;
+      }
+    }, time);
+    loop1 = setInterval(function () {
+      "use strict";
+      if (i > 100) {
+        clearInterval(loop1);
+        selector1.textContent = '4';
+      } else {
+        selector1.textContent = String(randomNum());
+        i++;
+      }
+    }, time);
+
   }
 
 }
